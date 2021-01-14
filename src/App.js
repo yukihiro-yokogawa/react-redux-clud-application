4c9ef635524dc91c8c7648de3141aa0c405a2744
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-01-14 16:13:40 +0900

    jsx plactice

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from "react";
 
 function App() {
-  return <div className="App"><h1>Hello, world!</h1></div>;
+  return <h1 className="App">Hello, world!</h1>;
 }
 
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world"
+//     );
+//   }
+// }
 export default App;
