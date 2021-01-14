b5ebf12510379699e1f0fcccd2074dcf9ab201b0
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-01-14 16:32:34 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React from "react";
 
 function App() {
-  return <h1 className="App">Hello, world!</h1>;
+  return (
+    <>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => {console.log("I am clicked")}}/>;
+    </>
+  )
 }
 
 // class App extends Component {
