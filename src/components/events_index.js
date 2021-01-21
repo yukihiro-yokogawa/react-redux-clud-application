c5c7756f78be2ce8b281fb35134d081b015eb19a
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-01-21 17:00:02 +0900

    implement event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 import { readEvents } from '../actions';
 
@@ -20,8 +21,8 @@ class EventsIndex extends Component {
   }
 
   render() {
-    console.log(this.props);
     return (
+      <React.Fragment>
         <table>   
           <thead>
             <tr>
@@ -34,6 +35,8 @@ class EventsIndex extends Component {
             {this.renderEvents()}
           </tbody>
         </table>
+        <Link to="/events/new">New Event</Link>
+      </React.Fragment>
     )
   }
 }
