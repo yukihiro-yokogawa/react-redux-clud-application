4b6f435c7fc302b93dd008ff646e42de2164e871
yukihiro.yokogawa@rakus-partners.co.jp <yukihiro.yokogawa@rakus-partners.co.jp>
2021-01-22 14:22:12 +0900

    implement dereat feature

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,42 +3,50 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
-import { postEvent } from '../actions';
+import { getEvent, deleteEvent, putEvent } from '../actions';
 
-class EventsNew extends Component {
+class EventsShow extends Component {
     constructor(props) {
         super(props);
         this.onSubmit = this.onSubmit.bind(this);
+        this.onDeleteClick = this.onDeleteClick.bind(this);
     }
 
     renderField(field) {
-        const { input, label, type, meta: {touched, error} } = field;
+        const { input, label, type, meta: { touched, error } } = field;
         return (
             <div>
-                <input {...input} placeholder={label} type={type}/>
+                <input {...input} placeholder={label} type={type} />
                 {touched && error && <span>{error}</span>}
             </div>
         )
     }
 
     async onSubmit(values) {
-        await this.props.postEvent(values);
+        //await this.props.postEvent(values);
+        this.props.history.push('/');
+    }
+
+    async onDeleteClick() {
+        const {id} = this.props.match.params;
+        await this.props.deleteEvent(id);
         this.props.history.push('/');
     }
 
     render() {
         const { handleSubmit, pristine, submitting } = this.props;
-        return(
+        return (
             <form onSubmit={handleSubmit(this.onSubmit)}>
                 <div>
-                    <Field label="Title" name="title" type="text" component={this.renderField}/>
+                    <Field label="Title" name="title" type="text" component={this.renderField} />
                 </div>
                 <div>
-                    <Field label="Body" name="body" type="text" component={this.renderField}/>
+                    <Field label="Body" name="body" type="text" component={this.renderField} />
                 </div>
                 <div>
                     <input type="submit" value="submit" disabled={pristine || submitting} />
                     <Link to="/">Cancel</Link>
+                    <Link to="/" onClick={this.onDeleteClick}>Delete</Link>
                 </div>
             </form>
         )
@@ -47,12 +55,12 @@ class EventsNew extends Component {
 
 const validate = values => {
     const errors = {}
-    if(!values.title) errors.title = "Enter a title, please."
-    if(!values.body) errors.body = "Enter a body, please."
+    if (!values.title) errors.title = "Enter a title, please."
+    if (!values.body) errors.body = "Enter a body, please."
     return errors;
 }
 
-const mapDispatchToProps = ({ postEvent })
+ const mapDispatchToProps = ({ deleteEvent })
 export default connect(null, mapDispatchToProps)(
-    reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+    reduxForm({ validate, form: 'eventShowForm' })(EventsShow)
 )
